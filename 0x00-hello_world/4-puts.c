28a03255b47d2d0cc06049688f996ca3b7623114
xom3e <moxomee@gmail.com>
2023-02-16 10:58:40 -0800

    the return

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,4 @@
-#include <studio.h>
+#include <stdio.h>
 /*
  * main - print a line of code using puts
  *
@@ -8,4 +8,4 @@ int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-
+}
